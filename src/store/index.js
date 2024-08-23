4fd12118a21a7066603838246c9f170bea6159d0
zedaaaaaaaaaaaa <dfederico961@gmail.com>
2024-08-23 00:14:05 -0300

    sssssada

---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import { createStore } from "vuex";
+
+export default createStore({
+  state: {
+    url: "http://localhost:3306/",
+  },
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {},
+});
